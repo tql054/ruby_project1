860bffdd426b2f49fc823c40b9b7926ced5b1e61
tql054 <tuanquocle2001@gmail.com>
2022-08-26 21:59:06 +0700

    first commit

---
+++ first_program.rb
@@ -0,0 +1,23 @@
+#Question 1
+fullName = "Le Quoc Tuan"
+studentCode = " 1911505310158"
+puts "Xin chao " + fullName + studentCode
+
+#Question 2
+puts "Nhap so: "
+number1 = gets
+puts "So vua nhap la: " + number1
+
+#Question 3
+puts "Nhap so thu nhat: "
+number2 = gets
+puts "Nhap so thu hai: "
+number3 = gets 
+
+if number1 > number2 
+    puts "So lon hon la: " + number1
+else 
+    puts "So lon hon la: " + number2
+end
+
+
